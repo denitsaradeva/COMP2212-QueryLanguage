a0fea695a5fc03a475fb597d6816139ab1ceca74
Teodor Pavlov <tosheca@icloud.com>
2022-03-23 18:20:19 +0000

    eval func foundation

--- Main.hs
+++ Main.hs
@@ -8,4 +8,9 @@ main = do
           y <- readFile (head x)
           print $ (parseCalc . alexScanTokens) y
           
+eval :: [(URIExp, URIExp, Object)] -> [TurtleExp] -> [(URIExp, URIExp, Object)]
+eval l [] = l
+eval l (x:xs) = eval (l ++ [(normalise x)]) xs
 
+normalise :: TurtleExp -> (URIExp, URIExp, Object)
+normalise (Triple e1 e2 e3) = (e1, e2, e3)
