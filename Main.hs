49adf6e9df73eadc045c288b733a3210600e1be5
Teodor Pavlov <tosheca@icloud.com>
2022-04-05 20:14:01 +0300

    eval func foundations

--- Main.hs
+++ Main.hs
@@ -9,14 +9,36 @@ main = do
           --print $ alexScanTokens y
           print $ (parseCalc . alexScanTokens) y
           
---eval :: [(URIExp, URIExp, Object)] -> [TurtleExp] -> [(URIExp, URIExp, Object)]
---eval l [] = l
---eval l (x:xs) = eval (l ++ [(normalise x)]) xs
+data Frame = TBase TurtleExp Environment | TPrefix TurtleExp Environment | TURIExpr String
+type Kontinuation = [ Frame ]
+type State = (TurtleExp,Environment,Kontinuation)
+type Environment = [ (String, TurtleExp) ]
 
-normalise :: TurtleExp -> [(URIExp, URIExp, Object)] -> [(URIExp, URIExp, Object)]
-normalise (Triple e1 e2 e3) ex = [(e1, e2, e3)]
-normalise (ObjMTriple e1 e2 []) ex = ex
-normalise (ObjMTriple e1 e2 (e:e3)) ex = normalise (ObjMTriple e1 e2 e3) (ex++[(e1, e2, e)])
-normalise (PredObjMTriple e1 []) ex = ex
-normalise (PredObjMTriple e1 (e:e2)) ex = normalise (PredObjMTriple e1 e2) (ex++(normalise (ObjMTriple e1 y z) []))
-                        where (y,z) = e
+--evalLoop :: TurtleExp -> TurtleExp
+--evalLoop e = evalLoop' (e,[],[])
+ -- where evalLoop' (e,env,k) = if (e' == e) && (isValue e') && (null k) then e' else evalLoop' (e',env',k')
+                    --   where (e',env',k') = eval (e,env,k) 
+
+--isValue :: Expr -> Bool
+--isValue (TmInt _) = True
+
+update :: Environment -> String -> TurtleExp -> Environment
+update env x e = (x,e) : env
+
+getValueBinding :: String -> Environment -> (TurtleExp,Environment)
+getValueBinding x [] = error "Variable binding not found"
+getValueBinding x ((y,e):env) | x == y  = unpack e
+                              | otherwise = getValueBinding x env
+
+unpack :: TurtleExp -> (TurtleExp,Environment)
+--unpack (Cl x t e env1) = ((TmLambda x t e) , env1)
+unpack e = (e,[])
+
+eval :: State -> State
+-- eval ((HardString x@("Base")),env,k) = (e',env',k)  where (e',env') = getValueBinding x env
+eval ((URIExpr x), env, k) =  ((AbsExpr ((getBaseValue $ getValueBinding "Base" env) ++ x)), env, k)
+eval ((Base e2@(AbsExpr e1)), env, k) = (e2, update env "Base" e1, k)
+
+
+getBaseValue :: (TurtleExp, Environment) -> String
+getBaseValue ((Base URIExpr e1), env) = e1
