76cd9e2e626397ffca4da41bf193fa087f2a2a21
Teodor Pavlov <tosheca@icloud.com>
2022-04-21 16:01:46 +0300

    First half of comments

--- Stql.hs
+++ Stql.hs
@@ -20,12 +20,13 @@ main = do
 
 -- Query Handle ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
+--Finds and returns the corresponding to the input name triples in our environment
 lookTurtleValue :: String -> TurtleEnv -> [(String, String, String)]
 lookTurtleValue a [] = error "non existing variable"
 lookTurtleValue a (x:xs) | a == (fst (x)) = snd (x)
                          | otherwise = lookTurtleValue a xs
 
+--Loops through every query line in the input query and handles its functionallity
 loopQuery :: [Query] -> TurtleEnv -> [(String, String, String)]
 loopQuery [] env = []
 loopQuery ((Where x):ys) env = loopQuery ys (filterConditionals (fst x) (snd x) env "")
@@ -35,32 +36,37 @@ loopQuery ((Print [x]):xs) env = (lookTurtleValue x env) ++ (loopQuery xs env)
 loopQuery ((Print (x:xs)):ys) env = (lookTurtleValue x env) ++ (loopQuery ((Print xs):ys) env)
 loopQuery (_:xs) env = loopQuery xs env
 
+--Updates the environment, as it puts a new tuple that is identical to the input one
 cloneEnvValue :: String -> String -> TurtleEnv -> TurtleEnv
 cloneEnvValue l1 l2 [] = []
 cloneEnvValue l1 l2 env = (l2, (lookTurtleValue l1 env)) : env
 
+--Handles the two types of update - substitution and change of value
 updateEnvValue :: String -> UpdateType -> TurtleEnv -> TurtleEnv
 updateEnvValue name (NormalUpdate x) env = (name, (updateTripleValue (fst x) (snd x) (lookTurtleValue name env))) : (removeTripleValue name env)
 updateEnvValue name (CalcUpdate x) env = (name, (calcUpdateTripleValue (fst x) (snd x) (lookTurtleValue name env))) : (removeTripleValue name env)
 
+--Removes an entry from the environment
 removeTripleValue :: String -> TurtleEnv -> TurtleEnv
 removeTripleValue name [] = []
 removeTripleValue name (x:env) | (fst x) == name = removeTripleValue name env
                                | otherwise = x : removeTripleValue name env
 
+--Handles the update query for change of value
 calcUpdateTripleValue :: Triplet -> LiteralType -> [(String, String, String)] -> [(String, String, String)]
 calcUpdateTripleValue Object (QPlusInt x) [] = []
---calcUpdateTripleValue Object (QPlusInt x) (y:ys) = (tripleFst y, tripleSnd y, (tripleTrd y)) : calcUpdateTripleValue Object (QPlusInt x) ys
 calcUpdateTripleValue Object (QPlusInt x) (y:ys) = (tripleFst y, tripleSnd y, (calcObjValue ("+"++(show x)) (tripleTrd y))) : calcUpdateTripleValue Object (QPlusInt x) ys
 calcUpdateTripleValue Object (QMinusInt x) [] = []
 calcUpdateTripleValue Object (QMinusInt x) (y:ys) = (tripleFst y, tripleSnd y, (calcObjValue ("-"++(show x)) (tripleTrd y))) : calcUpdateTripleValue Object (QMinusInt x) ys
 calcUpdateTripleValue x y z = error "Invalid request"
 
+--Calculates the upated object value
 calcObjValue :: String -> String -> String
 calcObjValue (x:xs) y | x == '+' = show ((read y :: Int) + (read xs :: Int))
                       | x == '-' = show ((read y :: Int) - (read xs :: Int))
                       | otherwise = error "Invalid input"
 
+--Handles the update query for substitution
 updateTripleValue :: Triplet -> LiteralType -> [(String, String, String)] -> [(String, String, String)]
 updateTripleValue Subject (QString x) [] = []
 updateTripleValue Subject (QString x) (y:ys) = (x, tripleSnd y, tripleTrd y) : updateTripleValue Subject (QString x) ys
@@ -74,9 +80,8 @@ updateTripleValue Object (QMinusInt x) (y:ys) = (tripleFst y, tripleSnd y, ("-"+
 updateTripleValue Object (QPlusInt x) (y:ys) = (tripleFst y, tripleSnd y, (show x)) : updateTripleValue Object (QPlusInt x) ys
 updateTripleValue x y z = error "Invalid request"
 
-  --(STring, string, string)
-
-filterConditionals :: String -> WhereType -> TurtleEnv -> String -> TurtleEnv -- last String is used for treshhold for AndWhereRequest
+--Handles all types of where query - and, or, and normal
+filterConditionals :: String -> WhereType -> TurtleEnv -> String -> TurtleEnv -- last String is used for threshold for AndWhereRequest
 filterConditionals name (NormalWhereRequest (Is a b)) env trshld = (name, (getFilteredIsTriples a b env trshld)) : env
 filterConditionals name (NormalWhereRequest (IsLit a b)) env trshld = (name, (getFilteredIsLitTriples a b env trshld)) : env
 filterConditionals name (NormalWhereRequest (IsBetween a b)) env trshld = (name, (getFilteredBetweenTriples a b env trshld)) : env
@@ -85,32 +90,38 @@ filterConditionals name (OrWhereRequest a b) env trshld = (name, ((lookTurtleVal
 filterConditionals name (AndWhereRequest a b) env trshld = filterConditionals name a e name
                                                       where e = filterConditionals name b env trshld
 
-getFilteredIsTriples :: (String, Triplet) -> (String, Triplet) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for treshhold for AndWhereRequest
+--Returns all triples that meet the input condition
+getFilteredIsTriples :: (String, Triplet) -> (String, Triplet) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for threshold for AndWhereRequest
 getFilteredIsTriples (n1, t1) (n2, t2) env trshld = (fixTriples t1 a t2 b) ++ (fixTriples t2 b t1 a)
                                                   where a | trshld == "" = lookTurtleValue n1 env
                                                           | otherwise = lookTurtleValue trshld env
                                                         b = lookTurtleValue n2 env
 
-getFilteredIsLitTriples :: (String, Triplet) -> LiteralType -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for treshhold for AndWhereRequest
+--Returns all triples, which are equal to the input literal value
+getFilteredIsLitTriples :: (String, Triplet) -> LiteralType -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for threshold for AndWhereRequest
 getFilteredIsLitTriples (n1, t1) l env trshld = (fixTriples' t1 a l)
                                                   where a | trshld == "" = lookTurtleValue n1 env
                                                           | otherwise = lookTurtleValue trshld env
 
-getFilteredBetweenTriples :: (String, Triplet) -> (LiteralType, LiteralType) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for treshhold for AndWhereRequest
+--Returns all triples that have int objects, falling between the input range
+getFilteredBetweenTriples :: (String, Triplet) -> (LiteralType, LiteralType) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for threshold for AndWhereRequest
 getFilteredBetweenTriples (n1, t1) (l, r) env trshld = (fixTriples'' l r a)
                                                        where a | trshld == "" = filterNumberObject $ lookTurtleValue n1 env
                                                                | otherwise = filterNumberObject $ lookTurtleValue trshld env
 
-getFilteredNotBetweenTriples :: (String, Triplet) -> (LiteralType, LiteralType) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for treshhold for AndWhereRequest
+--Returns all triples that have int objects, not falling between the input range
+getFilteredNotBetweenTriples :: (String, Triplet) -> (LiteralType, LiteralType) -> TurtleEnv -> String -> [(String, String, String)] -- last String is used for threshold for AndWhereRequest
 getFilteredNotBetweenTriples (n1, t1) (l, r) env trshld = (fixTriples''' l r a)
                                                        where a | trshld == "" = filterNumberObject $ lookTurtleValue n1 env
                                                                | otherwise = filterNumberObject $ lookTurtleValue trshld env
 
+--Returns all triples with numerical objects
 filterNumberObject :: [(String, String, String)] -> [(String, String, String)]
 filterNumberObject [] = []
 filterNumberObject (x:xs) | isInt (tripleTrd x) = x : filterNumberObject xs
                           | otherwise = filterNumberObject xs
 
+--Returns all triples, which match the input condition
 fixTriples :: Triplet -> [(String, String, String)] -> Triplet -> [(String, String, String)] -> [(String, String, String)]
 fixTriples t1 [] t2 [] = error "Empty inputs"
 
@@ -135,7 +146,7 @@ fixTriples Object (x:xs) Predicate ys = (filter ((==(tripleTrd x)).tripleSnd) ys
 fixTriples Object [] Object ys = []
 fixTriples Object (x:xs) Object ys = (filter ((==(tripleTrd x)).tripleTrd) ys) ++ (fixTriples Object xs Object ys)
 
---fixes triples for IsLit case
+--Fixes triples for IsLit case
 fixTriples' :: Triplet -> [(String, String, String)] -> LiteralType -> [(String, String, String)]
 fixTriples' t1 [] l = []
 fixTriples' Subject xs (QString l) = (filter ((==(l)).tripleFst) xs)
@@ -147,6 +158,7 @@ fixTriples' Object xs (QPlusInt l) = (filter ((==(show l)).tripleTrd) xs)
 fixTriples' Object xs (QMinusInt l) = (filter ((==("-"++show l)).tripleTrd) xs)
 fixTriples' t1 xs l = error "Invalid input"
 
+--Fixes triples for IsBetween case
 fixTriples'' :: LiteralType -> LiteralType -> [(String, String, String)] -> [(String, String, String)]
 fixTriples'' l1 l2 [] = []
 fixTriples'' (QInt a) (QInt b) xs | a > b = error "Invalid range"
@@ -163,6 +175,7 @@ fixTriples'' (QPlusInt a) (QInt b) xs  | a > b = error "Invalid range"
                                        | otherwise =  filter ((>=a) . (read')) (filter (((<=b) . (read'))) xs)
 fixTriples'' l1 l2 xs = error "Invalid range"
 
+--Fixes triples for IsNotBetween case
 fixTriples''' :: LiteralType -> LiteralType -> [(String, String, String)] -> [(String, String, String)]
 fixTriples''' l1 l2 [] = []
 fixTriples''' (QInt a) (QInt b) xs | a > b = error "Invalid range"
@@ -179,22 +192,24 @@ fixTriples''' (QPlusInt a) (QInt b) xs  | a > b = error "Invalid range"
                                         | otherwise =  filter ((<a) . (read')) xs ++ filter ((>b) . (read')) xs 
 fixTriples''' l1 l2 xs = error "Invalid range"
 
---find error case
+--Converts the object(third) value of the triple from String to Int. Filtering before conversion ensures no errors in parsing
 read' :: (String, String, String) -> Int
 read' x = read (tripleTrd x) :: Int 
 
-
+--Fills up our environment/memory with all files that would need turtle normalisation for our query
 scanForFileNames :: [Query] ->  [(String, String)]
 scanForFileNames [] = []
 scanForFileNames ((Select []):xs) = []
 scanForFileNames ((Select (a:ys)):xs) = (((fst a), (snd a)++".ttl") : (scanForFileNames (((Select (ys)):xs))))
 scanForFileNames (_ :xs) = []
 
+--Transforms a file path to IO String type
 readQFile :: FilePath -> IO String
 readQFile fileName = do 
                         text <- readFile fileName
                         return text
 
+--Reads all file names from our priorly filled up memory and normalises all turtle files
 loopNames :: [(String, String)] -> IO TurtleEnv
 loopNames [] = return []
 loopNames (file:fileNames) = do
@@ -203,13 +218,15 @@ loopNames (file:fileNames) = do
                                x <- (loopNames fileNames) -- IO [(String, String, String)]
                                return (z : x)
 
+--Transforms tuple into IO tuple type
 ask :: (String, [(String,String,String)]) -> IO (String, [(String,String,String)])
 ask s = return s
 
+--Checks whether a reference of a variable exists
 exists :: String -> TurtleEnv -> Bool
 exists c [] = False
 exists c (x:xs) | c == fst x = True
-                | otherwise = exists xs
+                | otherwise = exists c xs
 
 -- Turtle Handle ----------------------------------------------------------------------------------------------------------------------------------------------------------
 prettyPrint :: [(String, String, String)] -> String
@@ -220,12 +237,14 @@ prettyPrint (x:xs) | (take 7 (tripleTrd x)) == "http://" = "<" ++ (tripleFst x) 
                    | isInt (tripleTrd x) == False = "<" ++ (tripleFst x) ++ "><" ++ (tripleSnd x) ++ "> " ++ "\"" ++ (tripleTrd x) ++ "\"" ++ " .\n" ++ (prettyPrint xs)
                    | otherwise = "<" ++ (tripleFst x) ++ "><" ++ (tripleSnd x) ++ "> " ++ (tripleTrd x) ++ " .\n" ++ (prettyPrint xs)
 
+--Removes all duplicate triples 
 filterDuplicates :: [(String, String, String)] -> [(String, String, String)]
 filterDuplicates [] = []
 filterDuplicates [x] = [x]
 filterDuplicates (x:y:xs) | (x == y) = filterDuplicates (y:xs)
                           | otherwise = x : filterDuplicates (y:xs)
 
+--Checks whether a string represents a number
 isInt :: String -> Bool
 isInt [] = True
 isInt (x:xs) | x == '-' = isInt xs
@@ -233,12 +252,15 @@ isInt (x:xs) | x == '-' = isInt xs
              | isDigit x = isInt xs
              | otherwise = False
 
+--Returns the third value from a triple
 tripleTrd :: (a,a,a) -> a
 tripleTrd (x,y,z) = z
 
+--Returns the second value from a triple
 tripleSnd :: (a,a,a) -> a
 tripleSnd (x,y,z) = y
 
+--Returns the first value from a triple
 tripleFst :: (a,a,a) -> a
 tripleFst (x,y,z) = x
 
@@ -253,6 +275,12 @@ sortBeta (a, b, c) (x, y, z)
   | b < y = LT
   | b > y = GT
   | b == y = compare c z
+
+finaliseEnv :: [(String, String)] -> [(String, String)] -> [(String, String)]
+finaliseEnv [] [] = []
+finaliseEnv [] ys = removeEmptyTriples ys
+finaliseEnv xs [] = removeEmptyTriples xs
+finaliseEnv xs ys = (removeEmptyTriples xs) ++ (removeEmptyTriples ys)
 
 removeEmptyTriples :: [(String, String)] -> [(String, String)]
 removeEmptyTriples [] = []
@@ -277,12 +305,6 @@ findPrefixes :: TurtleExp -> [(String, String)] -> (String, String)
 findPrefixes (Prefix (pr) (AbsExpr st)) env = (pr, "http://"++st)
 findPrefixes (Prefix (pr) (URIExpr st)) env = (pr, ((lookValue "base" env)++st))
 findPrefixes _ env= ("", "")
-
-finaliseEnv :: [(String, String)] -> [(String, String)] -> [(String, String)]
-finaliseEnv [] [] = []
-finaliseEnv [] ys = removeEmptyTriples ys
-finaliseEnv xs [] = removeEmptyTriples xs
-finaliseEnv xs ys = (removeEmptyTriples xs) ++ (removeEmptyTriples ys)
 
 lookValue :: String -> [(String, String)] -> String
 lookValue a [] = error "non existing variable"
